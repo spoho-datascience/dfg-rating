ec002c13ed8bf3c55db5ea9bd3137ffaabebfddc
Marc Garnica Caparrós <marcgarnica13@gmail.com>
2021-03-01 08:50:59 +0100

    Fixed error with 0.0 home and away expected values
     - ELO rating was using 0.0 for expected values for each home and away
    team. This was resulting in a constant expected value of 0.5.
     - Error in getting the last value of the team using the current
    position in the Ratings array (0.0) instead of the - 1 (latest computed
    value).
     - Fixed error in ELORating class.
     - Error commented in story [ch331].

--- dfg_rating/model/rating/elo_rating.py
+++ dfg_rating/model/rating/elo_rating.py
@@ -38,6 +38,7 @@ class ELORating(BaseRating):
             ratings[team, init_position] = self.init_ratings(team, season, n, ratings)
 
     def compute_expected_values(self, home_value, away_value):
+        print(home_value, away_value)
         expected_home = 1.0 / (1.0 + (self.settings['c'] ** ((away_value - home_value) / self.settings['d'])))
         return expected_home, 1 - expected_home
 
@@ -77,8 +78,8 @@ class ELORating(BaseRating):
                     current_round = match_data['round']
                     current_position = (current_season * (self.rounds_per_season + 2)) + (current_round + 1)
                     home_expected, away_expected = self.compute_expected_values(
-                        ratings[home_team, current_position],
-                        ratings[away_team, current_position]
+                        ratings[home_team, current_position - 1],
+                        ratings[away_team, current_position - 1]
                     )
                     home_score, away_score = self.compute_scores(match_data['winner'])
                     ratings[away_team, current_position] = self.update_elo(
