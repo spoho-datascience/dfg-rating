46d533292bd006772d715645810b3dfdaa2792c8
Marc Garnica Caparrós <marcgarnica13@gmail.com>
2021-05-26 17:53:48 +0200

    Adding expected returns as a reference to true forecast

--- dfg_rating/model/evaluators/profitability.py
+++ dfg_rating/model/evaluators/profitability.py
@@ -9,7 +9,7 @@ class ProfitabilityEvaluator(Evaluator):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.outcomes = kwargs.get("outcomes")
-        self.player_name = kwargs.get("player_name")
+        self.true_model = kwargs.get("true_model")
         self.player_forecast = kwargs.get("player_forecast")
         self.bookmaker_name = kwargs.get("bookmaker_name")
 
@@ -21,14 +21,16 @@ class ProfitabilityEvaluator(Evaluator):
 class BettingReturnsEvaluator(ProfitabilityEvaluator):
 
     def eval(self, match_attributes):
+        ## Should be using the true model forecast for evaluation
         bets: List[float] = match_attributes['bets'][self.player_name]
-        bettor_predictions: List[float] = match_attributes['forecasts'][self.player_forecast].probabilities
+        # bettor_predictions: List[float] = match_attributes['forecasts'][self.player_forecast].probabilities
+        true_model: List[float] = match_attributes['forecasts'][self.true_model].probabilities
         bookmaker_odds: List[float] = match_attributes['odds'][self.bookmaker_name]
         observed_result = match_attributes.get('winner')
         expected_returns = []
         actual_returns = []
         for bet_index, bet in enumerate(bets):
-            expected_returns.append(bet * ((bettor_predictions[bet_index] * bookmaker_odds[bet_index]) - 1))
+            expected_returns.append(bet * ((true_model[bet_index] * bookmaker_odds[bet_index]) - 1))
             bet_result = 1.0 if self.outcomes[bet_index] == observed_result else 0.0
             actual_returns.append(bet * ((bet_result * bookmaker_odds[bet_index]) - 1))
         return 1, [(a, e) for a, e in zip(actual_returns, expected_returns)]
